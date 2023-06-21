a2f59d8f8162cc7398fd8bb06bc546e1bbc23ad1
Ali Can Yücel <yucelalican30@gmail.com>
2023-06-21 16:07:46 +0300

    MaqQuestSimilator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapSimilatorComponent } from './map-similator/map-similator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapSimilatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/map-similator/map-similator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-similator',
+  templateUrl: './map-similator.component.html',
+  styleUrls: ['./map-similator.component.scss']
+})
+export class MapSimilatorComponent {
+
+}
