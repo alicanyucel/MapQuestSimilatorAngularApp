b4a05ba8a8e8daeb8b23b4fe3010faac654a627c
Ali Can Yücel <yucelalican30@gmail.com>
2023-06-21 18:36:52 +0300

    ts bugs error

--- src/app/map-similator/map-similator.component.ts
+++ src/app/map-similator/map-similator.component.ts
@@ -1,92 +1,72 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-map-similator',
-  templateUrl: './map-similator.component.html',
-  styleUrls: ['./map-similator.component.scss']
+    selector: 'app-map-similator',
+    templateUrl: './map-similator.component.html',
+    styleUrls: ['./map-similator.component.scss']
 })
+
 export class MapSimilatorComponent {
- map = L.map('map', {
-  layers: MQ.mapLayer(),
-  center: [35.791188, -78.636755],
-  zoom: 12
-});
-  
+    map = L.map('map', {
+        layers: MQ.mapLayer(),
+        center: [40.038463, 32.903356], // Pursaklar A Life Hastanesi
+        zoom: 12
+    });
+   
+     dir = MQ.routing.directions();
+        dir.route({
+            locations: [
+                start,
+                end
+            ]
+        });
+     CustomRouteLayer = MQ.Routing.RouteLayer.extend({
+            createStartMarker: (location: any) => {
+                var custom_icon;
+                var marker;
+                custom_icon = L.icon({
+                    iconUrl: 'img/red.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+                marker = L.marker(location.latLng, { icon: custom_icon }).addTo(map);
+                return marker;
+            },
+            createEndMarker: (location: any) => {
+                var custom_icon;
+                var marker;
+                custom_icon = L.icon({
+                    iconUrl: 'img/blue.png',
+                    iconSize: [20, 29],
+                    iconAnchor: [10, 29],
+                    popupAnchor: [0, -29]
+                });
+                marker = L.marker(location.latLng, { icon: custom_icon }).addTo(map);
+                return marker;
+            }
+        });
+        map.addLayer(new CustomRouteLayer({
+            directions: dir,
+            fitBounds: true
+        }));
+    }
+    submitForm(event: any) {
+        event.preventDefault();
+        map.remove();
+        var start = document.getElementById("start").value;
+        var end = document.getElementById("destination").value;
 
-  public runDirection(start:any, end:any) {
-      
-      // recreating new map layer after removal
-     var map = L.map('map', {
-          layers: MQ.mapLayer(),
-          center: [35.791188, -78.636755],
-          zoom: 12
-      });
-      
-      var dir = MQ.routing.directions();
-
-      dir.route({
-          locations: [
-              start,
-              end
-          ]
-      });
-  
-
-    var  CustomRouteLayer = MQ.Routing.RouteLayer.extend({
-          createStartMarker: (location:any) => {
-              var custom_icon;
-              var marker;
-
-              custom_icon = L.icon({
-                  iconUrl: 'img/red.png',
-                  iconSize: [20, 29],
-                  iconAnchor: [10, 29],
-                  popupAnchor: [0, -29]
-              });
-
-              marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
-
-              return marker;
-          },
-
-          createEndMarker: (location:any) => {
-              var custom_icon;
-              var marker;
-
-              custom_icon = L.icon({
-                  iconUrl: 'img/blue.png',
-                  iconSize: [20, 29],
-                  iconAnchor: [10, 29],
-                  popupAnchor: [0, -29]
-              });
-
-              marker = L.marker(location.latLng, {icon: custom_icon}).addTo(map);
-
-              return marker;
-          }
-      });
-      
-      map.addLayer(new CustomRouteLayer({
-          directions: dir,
-          fitBounds: true
-      })); 
-  }
-
-
-// function that runs when form submitted
-submitForm(event:any) {
-  event.preventDefault();
-
-  // delete current map layer
-  map.remove();
-
-  // getting form data
-var start = document.getElementById("start").value;
-var end = document.getElementById("destination").value;
-
-var runDirection(start:any, end:any);
-document.getElementById("form").reset();
-}
-const form = document.getElementById('form');
+        var runDirection(start: any, end: any);
+        document.getElementById("form").reset();
+    }
+    const form = document.getElementById('form');
 form.addEventListener('submit', submitForm);
+}
+public runDirection(start: any, end: any) {
+    var map = L.map('map', {
+        layers: MQ.mapLayer(),
+        center: [35.791188, -78.636755],
+        zoom: 12
+    });
 }
